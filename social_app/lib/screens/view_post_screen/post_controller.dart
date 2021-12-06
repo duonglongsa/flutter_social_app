b374d07fa021f1d27105d4a49663f6b681f08a8b
= <=>
2021-12-06 11:58:14 +0700

    get list and post comment

---
+++ social_app/lib/screens/view_post_screen/post_controller.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
+import 'package:social_app/models/comment_model.dart';
+import 'package:social_app/models/post.dart';
+import 'package:social_app/services/auth.dart';
+import 'package:social_app/services/comment_service.dart';
+import 'package:social_app/services/post_service.dart';
+
+class PostController extends GetxController {
+
+  final  storage = const FlutterSecureStorage();
+  String ?userId, postId, token;
+  List<CommentModel> ?commentList;
+
+  TextEditingController commentController = TextEditingController();
+  
+  Future getUserInfo() async {
+    userId = await storage.read(key: 'userId');
+    token = await storage.read(key: 'token');
+  }
+
+  Future<List<CommentModel>> getList() async {
+    commentList = await getCommentList(postId!, token!);
+    commentList = commentList!.reversed.toList();   //newest post first
+    update();
+    print("update");
+    return commentList!;
+  }
+
+  // void removePostFromList(String postId){
+  //   postList!.removeWhere((item) => item.postID == postId);
+  //   update();
+  // }
+
+}
--- social_app/lib/services/comment_service.dart
+++ social_app/lib/services/comment_service.dart
@@ -8,9 +8,24 @@ import 'package:async/async.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:social_app/utilities/configs.dart';
 
-Future<String> createComment(Post post, String token, CommentModel comment) async {
- 
-  var res = await http.post(Uri.parse(localhost + "/v1/postComment/create/${post.postID}"),
+
+Future<List<CommentModel>> getCommentList(String postId, String token) async {
+  print("post ID: $postId");
+  var res = await http.get(Uri.parse(localhost + "/v1/postComment/list/" + postId),
+      headers: {
+        'Context-Type': 'application/json;charSet=UTF-8',
+        'Authorization': 'Bearer $token',
+        'Accept': 'application/json',
+      },);
+
+  var responseJson = json.decode(res.body);
+  print(responseJson["data"].toString());
+  return (responseJson["data"] as List).map((p) => CommentModel.fromJson(p)).toList();
+
+}
+
+Future<String> createComment(String postId, CommentModel comment, String token) async {
+  var res = await http.post(Uri.parse(localhost + "/v1/postComment/create/" + postId),
       headers: {
         'Context-Type': 'application/json;charSet=UTF-8',
         'Authorization': 'Bearer $token',
@@ -19,9 +34,12 @@ Future<String> createComment(Post post, String token, CommentModel comment) asyn
       },
       body: {
         "content": comment.content,
-        "commentAnswered": "",
+        "commentAnswered": comment.commentAnsweredId,
+        "index":"1",
+        "count":"10",
       });
-
-  print(res.body);
+  print(res.statusCode);
   return res.statusCode.toString();
 }
+
+
